062b10a3c3fae8e2a67d09e3d105ed5eb8de6b6b
menh <dinhthienmenh1505@gmail.com>
2025-06-15 23:55:54 +0700

    feat(ui): logo navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import Navbar from "./components/Navbar";
 
-import Home from "./pages/Home";
+import Home from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="min-h-screen bg-base-200 transition-colors duration-300">
+    <div
+      className="min-h-screen bg-base-200 transition-colors duration-300"
+      data-theme="forest"
+    >
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+import { ShoppingCartIcon } from "lucide-react";
+
+function Navbar() {
+  return (
+    <div className="bg-base-100/80 backdrop-blur-lg border-b border-base-content/10 sticky top-0 left-0 right-0 z-50">
+      <div className="max-w-7xl mx-auto">
+        <div className="navbar px-4 min-h-[4rem] justify-between">
+          <div className="flex-1 lg:flex-none">
+            <Link to={"/"} className="hover:opacity-80 transition-opacity">
+              <div className="flex items-center gap-2">
+                <ShoppingCartIcon className="size-9 text-primary" />
+                <span className="font-semibold font-mono tracking-widest text-2xl bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary">
+                  PRODUCT STORE
+                </span>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
